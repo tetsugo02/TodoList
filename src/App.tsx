120c41ab631f25d70467162dc6ab4ae995ed114c
tetsugo-mac <tetsugou2002@gmail.com>
2024-10-05 01:07:23 +0900

    Add AddNewTodoコンポーネント: 新しいTodoを追加するための入力フィールドとボタンを実装し、Appコンポーネントに組み込み

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import AddNewTodo from "./components/AddNew";
 import TodoList from "./components/TodoList";
 
 function App() {
 	return (
 		<>
 			<TodoList isDone={false} />
+			<AddNewTodo />
 		</>
 	);
 }
---
+++ src/components/AddNew.tsx
@@ -0,0 +1,10 @@
+const AddNewTodo = () => {
+	return (
+		<div>
+			<input type="text" placeholder="Add new todo" />
+			<button>Add</button>
+		</div>
+	);
+};
+
+export default AddNewTodo;
